d9cd907a05f9dffd597763247e5920113bb68c47
YusufMac <yusufonderd@gmail.com>
2021-05-12 12:25:43 +0300

    Add remote settings data source

---
+++ app/src/main/java/com/yonder/addtolist/data/remote/datasource/settings/RemoteSettingsDataSourceImpl.kt
@@ -0,0 +1,13 @@
+package com.yonder.addtolist.data.remote.datasource.settings
+
+import com.yonder.addtolist.data.remote.ApiService
+import javax.inject.Inject
+
+/**
+ * Yusuf Onder on 12,May,2021
+ */
+
+class RemoteSettingsDataSourceImpl @Inject constructor(private val apiService: ApiService) :
+  RemoteSettingsDataSource {
+
+}
