43eb68d84579b924e94e263462dd80eac1094d18
yusuf.onder <yusufonderder@gmail.com>
2021-11-17 22:03:20 +0300

    Switch compose in settings fragment

--- app/src/main/java/com/yonder/addtolist/scenes/settings/presentation/SettingsFragment.kt
+++ app/src/main/java/com/yonder/addtolist/scenes/settings/presentation/SettingsFragment.kt
@@ -1,30 +1,119 @@
 package com.yonder.addtolist.scenes.settings.presentation
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Divider
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
-import com.yonder.addtolist.common.ui.base.BaseFragment
-import com.yonder.addtolist.databinding.SettingsFragmentBinding
+import com.yonder.addtolist.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class SettingsFragment : BaseFragment<SettingsFragmentBinding>() {
+class SettingsFragment : Fragment() {
 
   val viewModel: SettingsViewModel by viewModels()
 
-  override fun initBinding(
-    inflater: LayoutInflater
-  ) = SettingsFragmentBinding.inflate(layoutInflater)
-
-  override fun initViews() {
-    binding.yoAccount.onClickLayout = {
-      findNavController().navigate(SettingsFragmentDirections.actionSettingsToAccountDetail())
-    }
-    binding.yoLanguage.onClickLayout = {
-      findNavController().navigate(SettingsFragmentDirections.actionSettingsToLanguageSelection())
+  override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View {
+    return ComposeView(requireContext()).apply {
+      setContent {
+        MainContent()
+      }
     }
   }
 
-  override fun initObservers() = Unit
+  @Composable
+  fun MainContent() {
+    val settingsUIState by viewModel.state.collectAsState()
+
+    when (settingsUIState) {
+      is SettingsUIState.Initial -> {
+        LazyColumn {
+          items((settingsUIState as SettingsUIState.Initial).imageDetailList) { imageDetail ->
+            TextButton(
+              onClick = {
+                when (imageDetail.titleResId) {
+                  R.string.account -> {
+                    findNavController().navigate(SettingsFragmentDirections.actionSettingsToAccountDetail())
+                  }
+                  R.string.language -> {
+                    findNavController().navigate(SettingsFragmentDirections.actionSettingsToLanguageSelection())
+                  }
+                }
+              }
+            ) {
+              Row(
+                modifier = Modifier
+                  .fillMaxSize()
+                  .padding(8.dp),
+                horizontalArrangement = Arrangement.Start,
+                verticalAlignment = Alignment.CenterVertically
+              ) {
+
+                imageDetail.leftPngResId?.let {
+                  Image(
+                    painter = painterResource(it),
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.padding(end = 8.dp)
+                  )
+                }
+
+                imageDetail.leftImageResId?.let {
+                  Icon(
+                    painter = painterResource(id = it),
+                    contentDescription = null,
+                    modifier = Modifier.padding(8.dp)
+                  )
+                }
+
+                imageDetail.leftTitleResId?.let {
+                  Text(
+                    style = MaterialTheme.typography.body1,
+                    text = getString(it),
+                    modifier = Modifier.padding(8.dp)
+                  )
+                }
+
+                Text(
+                  text = getString(imageDetail.titleResId),
+                  color = Color.DarkGray,
+                  style = MaterialTheme.typography.body1)
+              }
+
+            }
+            Divider(color = Color.LightGray)
+          }
+        }
+      }
+    }
+  }
 
 }
--- app/src/main/java/com/yonder/addtolist/scenes/settings/presentation/SettingsViewModel.kt
+++ app/src/main/java/com/yonder/addtolist/scenes/settings/presentation/SettingsViewModel.kt
@@ -1,8 +1,75 @@
 package com.yonder.addtolist.scenes.settings.presentation
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import androidx.lifecycle.ViewModel
+import com.yonder.addtolist.R
+import com.yonder.addtolist.core.data.State
+import com.yonder.addtolist.scenes.languageselection.LanguageSelectionViewEvent
+import com.yonder.addtolist.scenes.languageselection.data.model.LanguageUiModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 @HiltViewModel
-class SettingsViewModel @Inject constructor() : ViewModel()
+class SettingsViewModel @Inject constructor() : ViewModel() {
+
+  private val _state: MutableStateFlow<SettingsUIState> =
+    MutableStateFlow(
+      SettingsUIState.Initial(
+        provideList()
+      )
+    )
+
+  val state: StateFlow<SettingsUIState> get() = _state
+
+  private fun provideList(): List<ImageDetail> {
+    return listOf(
+      ImageDetail(
+        titleResId = R.string.account,
+        leftImageResId = R.drawable.ic_baseline_person_outline_24
+      ),
+      ImageDetail(titleResId = R.string.about, leftImageResId = R.drawable.ic_baseline_info_24),
+      ImageDetail(
+        titleResId = R.string.language,
+        leftImageResId = R.drawable.ic_baseline_language_24
+      ),
+      ImageDetail(
+        titleResId = R.string.rate_us,
+        leftImageResId = R.drawable.ic_baseline_rate_review_24
+      ),
+      ImageDetail(
+        titleResId = R.string.share_app,
+        leftImageResId = R.drawable.ic_baseline_share_24
+      ),
+      ImageDetail(
+        titleResId = R.string.be_premium,
+        leftTitleResId = R.string.premium_emo
+      ),
+      ImageDetail(
+        titleResId = R.string.help_us_to_translate_app,
+        leftTitleResId = R.string.world_emo
+      ),
+      ImageDetail(
+        titleResId = R.string.instagram,
+        leftPngResId = R.drawable.instagram_sketched
+      ),
+      ImageDetail(
+        titleResId = R.string.twitter,
+        leftPngResId = R.drawable.twitter
+      )
+    )
+  }
+}
+
+data class ImageDetail(
+  @StringRes var titleResId: Int,
+  @DrawableRes var leftImageResId: Int? = null,
+  @DrawableRes var leftPngResId : Int? = null,
+  @StringRes var leftTitleResId: Int? = null,
+)
+
+sealed class SettingsUIState {
+  data class Initial(val imageDetailList: List<ImageDetail>) : SettingsUIState()
+}
